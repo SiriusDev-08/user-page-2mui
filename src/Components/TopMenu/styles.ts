6715fdba7f5c7c7f97c4533b1131f5a6164b1588
Marcio <marciogomes@alu.ufc.br>
2022-04-06 16:08:13 -0300

    style: treino responsividade

--- src/Components/TopMenu/styles.ts
+++ src/Components/TopMenu/styles.ts
@@ -1,24 +1,17 @@
 import {  makeStyles } from "@material-ui/core"; 
 
 export const useStyles = makeStyles ({
-  container: {
-
-
-  },
-
+  
   appBar: {
 
     display: "flex",
+    justifyContent:"space-between", 
     width: "900px", 
     height: "64px",
     alignItems: "start", 
     padding: "0px 10px 10px 30px",
 
-    "@media (max-width: 1000px)":{
-      paddingLeft: "500px",
-      alignItems: "center",
-      paddingRight: "0px",
-    }
+   
     
   }, 
 
--- src/Pages/Home/Home.styles.ts
+++ src/Pages/Home/Home.styles.ts
@@ -3,18 +3,37 @@ import { height } from "@material-ui/system";
 
 export const useStyles = makeStyles ({
   container: {
-
+    display: "flex",
+    width: "100%",
+    minWidth:"100%", 
+    padding: 10,
+    backgroundColor: "#f3f3f3",
+    height: "100vh",
+    overflowX: "hidden",
     "@media (min-width: 1000px)":{
       height: "100%",
-    }  
+    }
+     
 
   },
 
   header: {
 
+    display: "flex",
+    flexWrap: "wrap",
     justifyContent: "center",
-    
 
+    "@media (min-width: 529px)":{
+      justifyContent: "space-between",
+    }, 
+    
+    paddingRight: "79px",
+    paddingLeft: "118px",
+    "@media (max-width:900px)":{
+      paddingLeft:0
+    }
+    
+    
   }, 
 
   footer: {
